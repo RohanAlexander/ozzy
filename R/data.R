c6dc091ec81b979d86fa35ef76a8223b806a7d79
Rohan Alexander <RohanAlexander@users.noreply.github.com>
2022-03-21 14:48:15 -0400

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' Favourite color of various people data
+#'
+#' @description \code{ozzy} returns a dataframe of favourt colos
+#'
+#' @return Returns a dataframe
+#'
+#' @docType data
+#'
+#' @usage data(color_data)
+#'
+#' @format A dataframe of individual-level observations.
+#'
+#' \describe{
+#' \item{\code{name}}{A character vector individual names.}}
+#' \item{\code{fav_color}}{One of: black, white, red}
+#' }
+#'
+#' @keywords datasets
+#'
+#' @source \{url{rohanalexander.com}}
+#'
+"color_data"
